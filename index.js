3eec309058bd99651f87360179da6d3c4dabdf1c
Fábio Mangan <fabio.mangan99@hotmail.com>
2024-12-29 19:26:29 -0300

    upgrade register

--- index.js
+++ index.js
@@ -1,95 +1,101 @@
 function onChangeEmail() {
-    toggleButtonsDisable();
-    toggleEmailErros();
+  toggleButtonsDisable();
+  toggleEmailErros();
 }
 
 function onChangePassword() {
-    toggleButtonsDisable();
-    togglePasswordErros();
+  toggleButtonsDisable();
+  togglePasswordErros();
 }
 
 function login() {
-    showLoading();
-    firebase.auth().signInWithEmailAndPassword(
-        form.email().value, form.password().value
-    ).then(response => {
-        hideLoading();
-        window.location.href = "pages/home/home.html";
-    }).catch(error => {
-        hideLoading();
-        alert(getErrorMessage(error));
+  showLoading();
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(form.email().value, form.password().value)
+    .then((response) => {
+      hideLoading();
+      window.location.href = "pages/home/home.html";
+    })
+    .catch((error) => {
+      hideLoading();
+      alert(getErrorMessage(error));
     });
 }
 
 function getErrorMessage(error) {
-    console.log(error)
-    if (error.code == "auth/invalid-credential") {
-        return "Usuário não encontrado";
-    }
-    return error.message;
+  console.log(error);
+  if (error.code == "auth/invalid-credential") {
+    return "Usuário não encontrado";
+  }
+  return error.message;
 }
 
 function register() {
-    window.location.href = "pages/register/register.html";
+  window.location.href = "pages/register/register.html";
 }
 
 function recoverPassword() {
-    showLoading();
-    firebase.auth().sendPasswordResetEmail(form.email().value).then(() => {
-        hideLoading();
-        alert('Email enviado com sucesso')
-    }).catch(error => {
-        hideLoading();
-        alert(getErrorMessage(error));
+  showLoading();
+  firebase
+    .auth()
+    .sendPasswordResetEmail(form.email().value)
+    .then(() => {
+      hideLoading();
+      alert("Email enviado com sucesso");
+    })
+    .catch((error) => {
+      hideLoading();
+      alert(getErrorMessage(error));
     });
 }
 
 function isEmailValid() {
-    const email = form.email().value;
-    if (!email) {
-        return false;
-    }
-    return validateEmail(email);
+  const email = form.email().value;
+  if (!email) {
+    return false;
+  }
+  return validateEmail(email);
 }
 
 function toggleEmailErros() {
-    const email = form.email().value;
-    form.emailRequiredError().style.display = email ? "none" : "block";
+  const email = form.email().value;
+  form.emailRequiredError().style.display = email ? "none" : "block";
 
-    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
+  form.emailInvalidError().style.display = validateEmail(email)
+    ? "none"
+    : "block";
 }
 
 function togglePasswordErros() {
-    const password = form.password().value
+  const password = form.password().value;
 
-    form.passwordRequiredError().style.display = password ? "none" : "block";
+  form.passwordRequiredError().style.display = password ? "none" : "block";
 }
 
 function toggleButtonsDisable() {
-    const emailValid = isEmailValid();
-    form.recoverPassword().disabled = !emailValid;
+  const emailValid = isEmailValid();
+  form.recoverPassword().disabled = !emailValid;
 
-    const passwordValid = isPasswordValid();
-    form.loginButton().disabled = !emailValid || !passwordValid;
+  const passwordValid = isPasswordValid();
+  form.loginButton().disabled = !emailValid || !passwordValid;
 }
 
 function isPasswordValid() {
-    const password = form.password().value;
-    if (!password) {
-        return false;
-    }
-    return true;
+  const password = form.password().value;
+  if (!password) {
+    return false;
+  }
+  return true;
 }
 
 const form = {
-    email: () => document.getElementById('email'),
-    emailInvalidError: () => document.getElementById('email-invalid-error'),
-    emailRequiredError: () => document.getElementById('email-required-error'),
-    loginButton: () => document.getElementById('login-button'),
-    password: () => document.getElementById('password'),
-    passwordRequiredError: () => document.getElementById('password-required-error'),
-    recoverPassword: () => document.getElementById('recover-password-button')
-}
-
-
-
+  email: () => document.getElementById("email"),
+  emailInvalidError: () => document.getElementById("email-invalid-error"),
+  emailRequiredError: () => document.getElementById("email-required-error"),
+  loginButton: () => document.getElementById("login-button"),
+  password: () => document.getElementById("password"),
+  passwordRequiredError: () =>
+    document.getElementById("password-required-error"),
+  recoverPassword: () => document.getElementById("recover-password-button"),
+};
